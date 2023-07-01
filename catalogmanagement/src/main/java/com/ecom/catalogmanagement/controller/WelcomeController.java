a05b52c310a8a86311f871f7cbaf8aa054cccbff
ravinderkumard <64346129+ravinderkumard@users.noreply.github.com>
2023-07-01 12:58:13 +0530

    Update WelcomeController.java

--- catalogmanagement/src/main/java/com/ecom/catalogmanagement/controller/WelcomeController.java
+++ catalogmanagement/src/main/java/com/ecom/catalogmanagement/controller/WelcomeController.java
@@ -10,6 +10,6 @@ public class WelcomeController {
 	
 	@GetMapping
 	public String welcome() {
-		return "Hello I am Controller 1 with Task Definition";
+		return "Hello I am Controller 1 with Task Definition Build 54 > Revision : 12";
 	}
 }
